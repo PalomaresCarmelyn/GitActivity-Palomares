7c11e3cc58ed05c0e885285b97528fa7167c6753
Palomares Carmelyn <carmelynpalomares1@gmail.com>
2023-03-09 17:49:16 -0800

    MyEducationalBackground

--- git-python-palomares.py
+++ git-python-palomares.py
@@ -1,3 +1,7 @@
 print("Carmelyn Palomares")
 print("August 24, 2003")
 print("Kaganhao Santa Cruz Marinduque")
+
+print("College of Information and Computing Science")
+print("Bachelor of Science Information Technology")
+print("2A")
